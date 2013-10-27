a4bbd9514ec3d49e7558c0849fdd7a761998e680
rushabh <rushabhone1@gmail.com>
2013-10-27 14:51:11 -0400

    modifying captcha

--- image.php
+++ image.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-
+echo 1
 $img = imagecreatetruecolor(150,70);
 $white = imagecolorallocate($img, 255, 255, 255);
 $black = imagecolorallocate($img, 0, 0, 0);
 $grey = imagecolorallocate($img,150,150,150);
 $red = imagecolorallocate($img, 255, 0, 0);
 $pink = imagecolorallocate($img, 200, 0, 150);
-
+echo 2
 function randomString($length){
     $chars = "abcdefghijklmnopqrstuvwxyz023456789";
     $str = "";
@@ -21,19 +21,22 @@ function randomString($length){
         }
     return $str;
 }
+echo 3
 for($i=1;$i<=rand(1,5);$i++){
     $color = (rand(1,2) == 1) ? $pink : $red;
     imageline($img,rand(0,150),rand(0,150), rand(15,70)+5,rand(15,50)+5, $color);
 }
+echo 4
 imagefill($img, 0, 0, $white);
 $string = randomString(rand(7,8));
 $_SESSION['string'] = $string;
 imagettftext($img, 15, 0, 10, 50, $black, "JC.ttf", $string);
 
+echo 4
 $_SESSION['secret_string'] = $string;
 
 header("Content-type: image/png");
 imagepng($img);
 imagedestroy($img);
-
+echo 5
 ?>
